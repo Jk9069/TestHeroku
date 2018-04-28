6efbbfea50c4964c0e656adfda34c5e33c112832
Jk9069 <cheok.jk9069@Gmail.com>
2018-04-28 22:39:42 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+#global var 
+app = Flask(__name__);
+
+#routes the app to webhook function
+@app.route('/WeatherWebhook', methods=['POST'])
+
+def WeatherWebhook():
+	#get post request
+	postReq = request.get_json(silent=True, force=True)
+
+	#prints out the post request (viewing purposes only)
+	print("Request:\n");
+	print(json.dumps(req, indent=4))
+
+	#pass the post request to another function to obtain a result from API
+	apiResult = getWebhookResult(postReq)
+
+	#print out result (viewing purposes only)
+	apiResult = json.dumps(apiResult, indent=4)
+	print(result)
+
+	#return result to chatbot to respond to user
+	finResult = make_response(apiResult)
+	finResult.headers['Content_Type'] = 'application/json'
+	return finResult
+
+def getWebhookResult(postReq):
+	if req.get("result").get("action") == "weather":
+		
+		speech = "The weather is clear";
+
+		return {
+			"speech": speech,
+			"displayText": speech,
+
+		}
+
+
+#main
+if __name__ = "__main__":
+	#port = int(os.getenv('PORT', 5000))
+	#print ("Starting app on port %d" %(port))
+	app.run(debug=True, host='0.0.0.0')
+
