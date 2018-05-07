f56bf3b7e5733312fb57c5e8649f9bdc83f99383
Jk9069 <cheok.jk9069@Gmail.com>
2018-05-07 15:06:01 +0800

    bug fix

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def getWebhookResult(postReq):
 		#making request to OpenWeather API		
 		#speech = "The weather is clear";
 		weatherUrl = "api.openweathermap.org/data/2.5/weather?q=Penang&APPID=5b39dc8cce894f4233c14ed2ad3d7c44&units=metric"
-		weatherResult = json.loads(urllib.urlopen(weatherUrl).read())
+		weatherResult = json.loads(urllib.request.urlopen(weatherUrl).read())
 
 		speech = "Today, the weather is " + weatherResult.get('weather').get('main')
 
