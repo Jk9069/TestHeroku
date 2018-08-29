23dbdf7b7afcbd1d9890b844bf2fed4878d052b3
Jk9069 <cheok.jk9069@Gmail.com>
2018-08-29 22:37:28 +0800

    test reading result from google places api response

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -16,6 +16,8 @@ class weatherPlaceRecommendations():
 		'shopping_mall', 'spa', 'points of interest', 'casino'
 	]
 
+	placeNames = []
+
 	def __init__ (self, weather):
 		global placeTypes
 		self.weather = weather
@@ -28,6 +30,22 @@ class weatherPlaceRecommendations():
 
 	#from placeTypes search for places in Google Places API
 	def requestPlaces(self):
+		global placeTypes, placeNames
+
+		#coordinates of penang: 5.285153 (lat), 100.456238 (long) - search Penang in general 
+		requestLink = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=5.4356,100.3091&radius=15000&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs"
+		
+		#generate random placeType and append to requestLink
+		requestLink = requestLink + "&type=" + placeTypes[random.randint(0, len(placeTypes))]
+		#post url
+		placeResult = json.loads(urllib.request.urlopen(requestLink).read())
+
+		#pluck information from placeResult, open now?
+		results = placeResult.get("results")
+		for items in results:
+			placeNames.append(item["name"])
+
+		#get place ID and get image, website
 
 		return {
 			"fulfillmentMessages": [
@@ -37,15 +55,25 @@ class weatherPlaceRecommendations():
 							"Okay, here goes nothing!"
 						]
 					}
+				},
+				{
+					"text": {
+						"text": [
+							placeNames[0]
+						]
+					}
+					#"card": {
+						#	"title": "name",
+						#	"subtitle": rating? address?,
+						#	"imageUri": "image"
+							#"buttons": [
+							#	{
+							#		"text": "More details",
+							#		"postback": "url "
+							#	}
+							#]
+						#}
 				}
-				#},
-				# {
-				# 	"text":{
-				# 		"text": [
-				# 			"Let me access your location"
-				# 		]
-				# 	}
-				# }
 			],
 
 			"source": 'Google Places API'
