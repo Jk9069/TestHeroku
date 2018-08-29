cd3bed0e142010b4ae6e6f66e3006498f6ad6386
Jk9069 <cheok.jk9069@Gmail.com>
2018-08-29 23:23:33 +0800

    modify global variable

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -8,35 +8,35 @@ from flask import request
 from flask import make_response
 
 class weatherPlaceRecommendations():
-	#array that stores places that user can go on sunny weather
-	placeTypes = [
-		'zoo', 'park', 'amusement_park', 'aquarium', 'art_gallery',
-		'bar', 'bowling_alley', 'cafe', 'department_store',
-		'library', 'movie_theater', 'museum', 'night_club', 'restaurant',
-		'shopping_mall', 'spa', 'points of interest', 'casino'
-	]
-
-	placeNames = []
-
-	def __init__ (self, weather):
-		global placeTypes
+	def __init__ (self, weather, placeTypes):
 		self.weather = weather
+		self.placeTypes = placeTypes
+
+	def definePlaceTypes(self):
+		#array that stores places that user can go on sunny weather
+		self.placeTypes = [
+			'zoo', 'park', 'amusement_park', 'aquarium', 'art_gallery',
+			'bar', 'bowling_alley', 'cafe', 'department_store',
+			'library', 'movie_theater', 'museum', 'night_club', 'restaurant',
+			'shopping_mall', 'spa', 'points of interest', 'casino'
+		]
 
 		#remove outdoor places from recommendations
 		if ('Rain' in self.weather or 'Thunderstorm' in self.weather):
-			placeTypes.remove('zoo')
-			placeTypes.remove('park')
-			placeTypes.remove('amusement_park')
+			self.placeTypes.remove('zoo')
+			self.placeTypes.remove('park')
+			self.placeTypes.remove('amusement_park')
+
 
 	#from placeTypes search for places in Google Places API
 	def requestPlaces(self):
-		global placeTypes, placeNames
+		placeNames = []
 
 		#coordinates of penang: 5.285153 (lat), 100.456238 (long) - search Penang in general 
 		requestLink = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=5.4356,100.3091&radius=15000&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs"
 		
 		#generate random placeType and append to requestLink
-		requestLink = requestLink + "&type=" + placeTypes[random.randint(0, len(placeTypes))]
+		requestLink = requestLink + "&type=" + self.placeTypes[random.randint(0, len(self.placeTypes))]
 		#post url
 		placeResult = json.loads(urllib.request.urlopen(requestLink).read())
 
