829aaeda8e29e7a1f92bd959da1ccc32ef5c868c
Jk9069 <cheok.jk9069@Gmail.com>
2018-08-29 23:28:17 +0800

    bug fixing

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -8,13 +8,12 @@ from flask import request
 from flask import make_response
 
 class weatherPlaceRecommendations():
-	def __init__ (self, weather, placeTypes):
+	def __init__ (self, weather):
 		self.weather = weather
-		self.placeTypes = placeTypes
 
 	def definePlaceTypes(self):
 		#array that stores places that user can go on sunny weather
-		self.placeTypes = [
+		placeTypes = [
 			'zoo', 'park', 'amusement_park', 'aquarium', 'art_gallery',
 			'bar', 'bowling_alley', 'cafe', 'department_store',
 			'library', 'movie_theater', 'museum', 'night_club', 'restaurant',
@@ -23,20 +22,24 @@ class weatherPlaceRecommendations():
 
 		#remove outdoor places from recommendations
 		if ('Rain' in self.weather or 'Thunderstorm' in self.weather):
-			self.placeTypes.remove('zoo')
-			self.placeTypes.remove('park')
-			self.placeTypes.remove('amusement_park')
+			placeTypes.remove('zoo')
+			placeTypes.remove('park')
+			placeTypes.remove('amusement_park')
+
+		return placeTypes
 
 
 	#from placeTypes search for places in Google Places API
 	def requestPlaces(self):
+		definedTypes = definePlaceTypes();
+
 		placeNames = []
 
 		#coordinates of penang: 5.285153 (lat), 100.456238 (long) - search Penang in general 
 		requestLink = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=5.4356,100.3091&radius=15000&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs"
 		
 		#generate random placeType and append to requestLink
-		requestLink = requestLink + "&type=" + self.placeTypes[random.randint(0, len(self.placeTypes))]
+		requestLink = requestLink + "&type=" + definedTypes[random.randint(0, len(definedTypes))]
 		#post url
 		placeResult = json.loads(urllib.request.urlopen(requestLink).read())
 
