e4aa6ae788eb104aad1139a2e774ac73b2210e00
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-01 22:02:05 +0800

    Revert "fix KeyError"
    This reverts commit 5342b3181adc07a5fe70e023c69692ebfca4f07d.

--- app.py
+++ app.py
@@ -51,17 +51,17 @@ def getWebhookResult(postReq):
 
 	#user responded 'yes' to obtain place suggestions
 	elif postedReq.get("action") == "GetWeather.GetWeather-yes":
-		outputContextsData = json.loads(postReq)
-		# for item in outputContexts:
-		# 	parameters = item["parameters"]
+		outputContexts = postedReq.get("outputContexts")
+		for item in outputContexts:
+			parameters = item["parameters"]
 
 		#obtained weather condition saved from prev intent, 
-		weather = outputContextsData['queryResult']['outputContexts'][2]['parameters']['mainWeather']
+		weather = parameters.get("mainWeather")
 		print(weather)
 		
 		#based on weather condition, decide what kind of place to suggest
 		weatherRecommend = weatherRecommendations.weatherPlaceRecommendations()
-		return weatherRecommend.requestPlaces('Rain')
+		return weatherRecommend.requestPlaces(weather)
 					
 
 #main
