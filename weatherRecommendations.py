ed56c289c86dd738245948b4177179a2b2130ca4
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-01 22:01:51 +0800

    Revert "still unable to read from json"
    This reverts commit 5bdde4457fc590baa38e481b80a10b1e82464afc.

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -50,11 +50,10 @@ class weatherPlaceRecommendations():
 		#post url, with headers for sygic travel api key
 		request = urllib.request.Request(requestLink, headers={'x-api-key':'MbSr78YZnbagZpgKINcfb16CcksWk7zyIF8FMzm5'})
 		placeResult = (urllib.request.urlopen(request)).read()
-		jsonResult = json.loads(placeResult.decode('utf8'))
 
-		s = json.dumps(jsonResult, indent=4, sort_keys=True)
+		jsonResult = json.dumps(json.loads(placeResult.decode('utf8')), indent=4, sort_keys=True)
 
-		print(s)
+		print(jsonResult)
 
 		#if there are results
 		responseText = ""
