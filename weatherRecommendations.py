74f915468b3a8029759bbf83643e60bfe1e26000
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-01 17:18:28 +0800

    removed bounds parameter, try to convert bytes to json

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -31,7 +31,8 @@ class weatherPlaceRecommendations():
 
 
 		#generate random placeCategory and append to requestLink
-		requestLink = "https://api.sygictravelapi.com/1.0/en/places/list?parents=city:3350&bounds=5.237559,100.347503,5.479325,100.173052&limit=20"
+		#&bounds=5.237559,100.347503,5.479325,100.173052
+		requestLink = "https://api.sygictravelapi.com/1.0/en/places/list?parents=city:3350&limit=20"
 
 		#"level=poi&"
 		#get random place category 
@@ -46,13 +47,14 @@ class weatherPlaceRecommendations():
 		#post url, with headers for sygic travel api key
 		request = urllib.request.Request(requestLink, headers={'x-api-key':'MbSr78YZnbagZpgKINcfb16CcksWk7zyIF8FMzm5'})
 		placeResult = (urllib.request.urlopen(request)).read()
+		jsonResult = json.loads(placeResult.decode('utf-8'))
 
 		print(placeResult)
 
 		#if there are results
 		responseText = ""
 		if (placeResult.get("status_code") == 200):
-			responseText = "Okay, here goes nothing!"
+			responseText = "Here it comes!"
 
 			#pluck information from placeResult, open now?
 			results = placeResult.get("data").get("places")
