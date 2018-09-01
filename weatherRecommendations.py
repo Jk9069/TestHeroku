f5367acdd08d69751a507722fa0a4ab7adec246d
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-01 14:16:42 +0800

    testing http headers

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -8,6 +8,7 @@ from flask import request
 from flask import make_response
 
 class weatherPlaceRecommendations():
+
 	#from placeTypes search for places in Google Places API
 	def requestPlaces(self, weather):
 		placeTypes = [
@@ -22,50 +23,52 @@ class weatherPlaceRecommendations():
 			placeTypes.remove('park')
 			placeTypes.remove('amusement_park')
 
-		#weather not printed?
-		print(weather)
-		print(len(placeTypes)) #shuold be 17 if weather is sunnt
+		#print(weather)
+		#shuold be 17 if weather is sunny, 15 if rainy
+		#print(len(placeTypes)) 
 
 		#coordinates of penang: 5.4356 (lat), 100.3091 (long) - search Penang in general 
 		#generate random placeTypes and append to requestLink
-		requestLink = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=5.4356,100.3091&radius=15000&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs"
-		requestLink = (requestLink + "&type=" + placeTypes[random.randint(0, len(placeTypes))])
+		response.header["x-api-key"] = "MbSr78YZnbagZpgKINcfb16CcksWk7zyIF8FMzm5"
+		requestLink = "https://api.sygictravelapi.com/1.0/en/places/list?parents=city:186&level=poi&limit=100"
+		#requestLink = (requestLink + "&type=" + placeTypes[random.randint(0, len(placeTypes))])
 
+		#so that can view actual results on web browser
 		print(requestLink)
 		
-		#post url
-		placeResult = json.loads(urllib.request.urlopen(requestLink).read())
+		# #post url
+		# placeResult = json.loads(urllib.request.urlopen(requestLink).read())
 
-		print(placeResult)
+		# print(placeResult)
 
-		responseText = ""
+		# #if there are results
+		# responseText = ""
+		# if (placeResult.get("status") == "OK"):
+		# 	responseText = "Okay, here goes nothing!"
+		# 	#pluck information from placeResult, open now?
+		# 	results = placeResult.get("results")
 
-		#if there are results
-		if (placeResult.get("status") == "OK"):
-			responseText = "Okay, here goes nothing!"
-			#pluck information from placeResult, open now?
-			results = placeResult.get("results")
+		# 	print(results)
 
-			print(results)
+		# 	for items in results:
+		# 		print(item["name"])
 
-			for items in results:
-				print(item["name"])
+		# elif (placeResult.get("status") == "ZERO_RESULTS"):
+		# 	responseText = "No results found :("
+		# 	#???
 
-		elif (placeResult.get("status") == "ZERO_RESULTS"):
-			responseText = "No results found :("
-			#???
+		# elif (placeResult.get("status") == "OVER_QUERY_LIMIT"):
+		# 	responseText = "Over query limit. Please try again in a few moments"
 
-		elif (placeResult.get("status") == "OVER_QUERY_LIMIT"):
-			responseText = "Over query limit. Please try again in a few moments"
-
-		#get place ID and get image, website
+		# #get place ID and get image, website
 
 		return {
 			"fulfillmentMessages": [
 				{
 					"text":{
 						"text":[
-							responseText
+							#responseText
+							"Here goes nothing..."
 						]
 					}
 				}
