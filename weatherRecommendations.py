5bdde4457fc590baa38e481b80a10b1e82464afc
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-01 17:55:02 +0800

    still unable to read from json

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -50,10 +50,11 @@ class weatherPlaceRecommendations():
 		#post url, with headers for sygic travel api key
 		request = urllib.request.Request(requestLink, headers={'x-api-key':'MbSr78YZnbagZpgKINcfb16CcksWk7zyIF8FMzm5'})
 		placeResult = (urllib.request.urlopen(request)).read()
+		jsonResult = json.loads(placeResult.decode('utf8'))
 
-		jsonResult = json.dumps(json.loads(placeResult.decode('utf8')), indent=4, sort_keys=True)
+		s = json.dumps(jsonResult, indent=4, sort_keys=True)
 
-		print(jsonResult)
+		print(s)
 
 		#if there are results
 		responseText = ""
