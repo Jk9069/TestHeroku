96c46ed17a4a10ac1e4ed07302d7515016a8fdd1
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-01 16:33:53 +0800

    implement api usage

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -9,67 +9,70 @@ from flask import make_response
 
 class weatherPlaceRecommendations():
 
-	#from placeTypes search for places in Google Places API
+	#from placeCategory search for places in Google Places API
 	def requestPlaces(self, weather):
-		placeTypes = [
-			'park', 'amusement_park', 'aquarium', 'art_gallery',
-			'bar', 'bowling_alley', 'cafe', 'department_store',
-			'library', 'movie_theater', 'museum', 'night_club', 'restaurant',
-			'shopping_mall', 'spa', 'points of interest', 'casino'
+		stayIndoor = False
+
+		#categories and tags
+		placeCategory = [
+			'discovering', 'eating', 'going_out', 'hiking',
+			'playing', 'relaxing', 'shopping', 'sightseeing',
+			'doing_sports', 'traveling'			
 		]
 
-		#remove outdoor places from recommendations
+		# placeTags = [
+		# 	''
+
+		# ]
+
 		if ('Rain' in weather or 'Thunderstorm' in weather):
-			placeTypes.remove('park')
-			placeTypes.remove('amusement_park')
+			#append indoor tag?
+			stayIndoor = True
 
-		#print(weather)
-		#shuold be 17 if weather is sunny, 15 if rainy
-		#print(len(placeTypes)) 
 
-		#coordinates of penang: 5.4356 (lat), 100.3091 (long) - search Penang in general 
-		#generate random placeTypes and append to requestLink
-		requestLink = "https://api.sygictravelapi.com/1.0/en/places/list?parents=city:186&level=poi&limit=100"
-		#requestLink = (requestLink + "&type=" + placeTypes[random.randint(0, len(placeTypes))])
+		#generate random placeCategory and append to requestLink
+		requestLink = "https://api.sygictravelapi.com/1.0/en/places/list?parents=city:3350&bounds=5.237559,100.347503,5.479325,100.173052&limit=20"
+
+		#"level=poi&"
+		#get random place category 
+		requestLink = requestLink + "&categories=" + placeCategory[random.randint(0, len(placeCategory))]
+
+		if (stayIndoor == True)
+			requestLink = requestLink + "&tags=indoor"
 
 		#so that can view actual results on web browser
 		print(requestLink)
-		
-		#post url
-		#placeResult = json.loads(urllib.request.urlopen(requestLink).read())
+	
+		#post url, with headers for sygic travel api key
 		request = urllib.request.Request(requestLink, headers={'x-api-key':'MbSr78YZnbagZpgKINcfb16CcksWk7zyIF8FMzm5'})
 		placeResult = (urllib.request.urlopen(request)).read()
 
 		print(placeResult)
 
-		# #if there are results
-		# responseText = ""
-		# if (placeResult.get("status") == "OK"):
-		# 	responseText = "Okay, here goes nothing!"
-		# 	#pluck information from placeResult, open now?
-		# 	results = placeResult.get("results")
+		#if there are results
+		responseText = ""
+		if (placeResult.get("status_code") == 200):
+			responseText = "Okay, here goes nothing!"
 
-		# 	print(results)
+			#pluck information from placeResult, open now?
+			results = placeResult.get("data").get("places")
 
-		# 	for items in results:
-		# 		print(item["name"])
+			for items in results:
+				print(item["name"])
 
-		# elif (placeResult.get("status") == "ZERO_RESULTS"):
-		# 	responseText = "No results found :("
-		# 	#???
+		#what about when no results found?
+		else:
+			responseText = "No results found :("
 
-		# elif (placeResult.get("status") == "OVER_QUERY_LIMIT"):
-		# 	responseText = "Over query limit. Please try again in a few moments"
 
-		# #get place ID and get image, website
+		#get place ID and get image, website
 
 		return {
 			"fulfillmentMessages": [
 				{
 					"text":{
 						"text":[
-							#responseText
-							"Here goes nothing..."
+							responseText
 						]
 					}
 				}
