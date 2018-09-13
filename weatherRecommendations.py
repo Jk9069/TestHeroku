70a23fbb15475a5f9d064993ae5dce7d742aff31
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-13 22:30:32 +0800

    trying to remove emoji from text

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -2,6 +2,7 @@ import urllib
 import json
 import os
 import random
+import re
 
 #Python Image Library
 from PIL import Image 
@@ -51,6 +52,8 @@ class weatherPlaceRecommendations():
 		categoryText = selectedCategory.replace('_', ' ')
 		if categoryText == 'library':
 			categoryText = 'libraries'
+		elif categoryText == 'art_gallery':
+			categoryText = 'art_galleries'
 		else:
 			categoryText = categoryText + 's'
 
@@ -189,19 +192,13 @@ class weatherPlaceRecommendations():
 		return self.readnFormatResults(placeResult, chosenCategory)
 
 	def remove_emoji(self, data):
-	    """
-	    去除表情
-	    :param data:
-	    :return:
-	    """
-	    if not data:
-	        return data
-	    if not isinstance(data, basestring):
-	        return data
-	    try:
-	    # UCS-4
-	        patt = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
-	    except re.error:
-	    # UCS-2
-	        patt = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
-	    return patt.sub('', data)
+	    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+
+		print(emoji_pattern.sub(r'', data)) # no emoji
+		
+		return emoji_pattern.sub(r'', data)
