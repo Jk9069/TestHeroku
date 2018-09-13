0fc083e6e5399f31b1d357831a3d53052baaa1fb
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-13 22:40:15 +0800

    re-indent

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -192,13 +192,15 @@ class weatherPlaceRecommendations():
 		return self.readnFormatResults(placeResult, chosenCategory)
 
 	def remove_emoji(self, data):
-	    emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags=re.UNICODE)
+		emoji_pattern = re.compile(
+			"["
+			u"\U0001F600-\U0001F64F"  # emoticons
+			u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+			u"\U0001F680-\U0001F6FF"  # transport & map symbols
+			u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+			"]+", flags=re.UNICODE
+		)
 
-		print(emoji_pattern.sub(r'', data)) # no emoji
+		#print(emoji_pattern.sub(r'', data))
 		
 		return emoji_pattern.sub(r'', data)
