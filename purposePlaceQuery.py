bf7ded7a4f5261681ce3de11c75c2e96b11bdf6a
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-13 22:11:13 +0800

    added function to remove emoji and function to format json result

--- purposePlaceQuery.py
+++ purposePlaceQuery.py
@@ -28,44 +28,120 @@ class purposePlaceQuery():
 		print(requestLink)
 		print(s)
 		
-		results = placeResult.get("results")
-		counter = 0;
-		listPlaceNames = []
-
-		for items in results:
-			placeName = items["name"]
-			listPlaceNames.append(placeName)
-			counter += 1
-
-			if counter > 9:
-				break;
-
-		return {
-			"fulfillmentText": "This is optional",
-			"fulfillmentMessages": [
-				{
-					"text": {
-						"text": [
-							listPlaceNames[0]
-						]
-					}
-				},
-				{
-					"text": {
-						"text": [
-							listPlaceNames[1]
-						]
-					}
-				},
-				{
-					"text": {
-						"text": [
-							listPlaceNames[2]
-						]
-					}
-				}
-			],
-			"source": "Google Places API"
-		}
+		return self.readnFormatResults(placeResult)
 
 	#function to read and format json results? 
+	def readnFormatResults(self, placeResult):
+		responseText = ""
+		shortlistPlaces = []
+
+		#if there are results
+		if (placeResult.get("status") == "OK"):
+			#responseText = "Okay, showing " + categoryText 
+			responseText = "Your purpose is " + self.travelPurpose 
+
+			#pluck information from placeResult, open now?
+			#get place ID and get image, website
+			results = placeResult.get("results")
+			counter = 0;
+
+			for items in results:
+				#only get places that are opened now?
+				if ("opening_hours" in items):
+					openNow = items["opening_hours"].get("open_now", 'false')
+				else:
+					openNow = 'false'
+				
+				placeName = items["name"]
+				placeID = items["place_id"]
+
+				if ("rating" in items):
+					rating = items["rating"]
+				else:
+					rating = '0'
+
+				#obtain photo reference to get image to display in cards
+				if ("photos" in items):
+					photoDeets = items["photos"]
+
+					for x in photoDeets:
+						if ("photo_reference" in x):
+							photoRef = x.get("photo_reference", 'none')
+
+				#using photo reference to get image
+				if (photoRef != 'none'):
+					photoRequest = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs&photoreference=" + photoRef
+					photoURL = urllib.request.urlopen(photoRequest).geturl()
+					#print(photoURL)
+
+				else:
+					photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
+
+				#maybe i should go get types?
+				stringTypes = ""
+				if ("types" in items):
+					types = items["types"]
+
+					for x in types:
+						stringTypes += x + ", "
+
+					#remove last 2 characters of the string
+					stringTypes = stringTypes[:-2]
+					
+					if '_' in stringTypes:
+						stringTypes = stringTypes.replace('_', " ")
+
+				#create the Place object containing all required values
+				newPlace = Place(placeID, placeName, rating, openNow, photoRef, photoURL, stringTypes)
+
+				#add to array to be displayed
+				if (counter < 10):
+					shortlistPlaces.append(newPlace)
+					counter += 1
+				else:
+					break
+				
+			googleLogo = Image.open("powered_by_google_on_white.png")
+			data = {
+				"source": "Google Places API", 
+				"fulfillmentMessages":[
+					{
+						"text":{
+							"text":[
+								responseText
+							]
+						}
+					} 
+				]
+			}
+
+			for x in range(len(shortlistPlaces)-1):
+				place = shortlistPlaces[x]
+
+				data["fulfillmentMessages"].append(
+					{
+						"card": { 
+							 "title": place.getPlaceName(),
+							 "subtitle": place.getRating() + "\n" + place.getOpenNow() + "\n" + place.getPlaceTypes(), #+ "\n" + googleLogo.show(),
+							 "imageUri": place.getPhotoURL(),
+							 "buttons": [
+							 	{
+							 		"text": "Map",
+							 		#link to open in google maps
+							 		"postback": "https://www.google.com/maps/search/?api=1&query=" + place.getPlaceName() + "&query_place_id=" + place.getPlaceID()
+							 	}
+							 ]
+						}
+					}
+				)
+
+		elif (placeResult.get("status") == "ZERO_RESULTS"):
+			responseText = "No results found :("
+
+		elif (placeResult.get("status") == "OVER_QUERY_LIMIT"):
+			responseText = "Over query limit. Please try again in a few moments"
+
+		else: 
+			responseText = ""
+
+		return data
--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -167,6 +167,7 @@ class weatherPlaceRecommendations():
 	def requestMore(self, chosenCategory):
 		#have to remove emojis before appending to the requestLink
 		chosenCategory = chosenCategory.replace(' ', '_')
+		chosenCategory = self.remove_emoji(chosenCategory)
 		print ("CHOSEN CATEGORY IS: " + chosenCategory)
 
 		#just in case things get complicated and this happensx
@@ -187,3 +188,20 @@ class weatherPlaceRecommendations():
  
 		return self.readnFormatResults(placeResult, chosenCategory)
 
+	def remove_emoji(self, data):
+	    """
+	    去除表情
+	    :param data:
+	    :return:
+	    """
+	    if not data:
+	        return data
+	    if not isinstance(data, basestring):
+	        return data
+	    try:
+	    # UCS-4
+	        patt = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+	    except re.error:
+	    # UCS-2
+	        patt = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
+	    return patt.sub('', data)
