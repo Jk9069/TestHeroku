c08bf3bb3d6d46134c0bb7a93e3b3273b4852a66
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-19 21:01:44 +0800

    add option to view more results in cards

--- weatherRecommendations.py
+++ weatherRecommendations.py
@@ -86,66 +86,78 @@ class weatherPlaceRecommendations():
 			results = placeResult.get("results")
 			counter = 0;
 
-			for items in results:
-				if ("opening_hours" in items):
-					openNow = items["opening_hours"].get("open_now")
-					# print(openNow)
-				else:
-					openNow = 'false'
-				
-				placeName = items["name"]
-				placeID = items["place_id"]
+			if (counter < 9):
+				if (counter != 8):
+					for items in results:
+						if ("opening_hours" in items):
+							openNow = items["opening_hours"].get("open_now")
+							# print(openNow)
+						else:
+							openNow = 'false'
+						
+						placeName = items["name"]
+						placeID = items["place_id"]
 
-				if ("rating" in items):
-					rating = items["rating"]
-				else:
-					rating = '0'
+						if ("rating" in items):
+							rating = items["rating"]
+						else:
+							rating = '0'
 
-				#obtain photo reference to get image to display in cards
-				if ("photos" in items):
-					photoDeets = items["photos"]
+						#obtain photo reference to get image to display in cards
+						if ("photos" in items):
+							photoDeets = items["photos"]
 
-					for x in photoDeets:
-						if ("photo_reference" in x):
-							photoRef = x.get("photo_reference", 'none')
+							for x in photoDeets:
+								if ("photo_reference" in x):
+									photoRef = x.get("photo_reference", 'none')
+								else:
+									photoRef = 'none'
+
+							#using photo reference to get image
+							if (photoRef != 'none'):
+								photoRequest = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs&photoreference=" + photoRef
+								photoURL = urllib.request.urlopen(photoRequest).geturl()
+								#print(photoURL)
+
+							else:
+								photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
 						else:
 							photoRef = 'none'
+							photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
 
-					#using photo reference to get image
-					if (photoRef != 'none'):
-						photoRequest = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyBMfB2YS4eye4FNNWvyv71DV5HN3ld8GDs&photoreference=" + photoRef
-						photoURL = urllib.request.urlopen(photoRequest).geturl()
-						#print(photoURL)
+						# get type that categorise the places
+						stringTypes = ""
+						if ("types" in items):
+							types = items["types"]
 
-					else:
-						photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
+							for x in types:
+								stringTypes += x + ", "
+
+							#remove last 2 characters of the string
+							stringTypes = stringTypes[:-2]
+							
+							if '_' in stringTypes:
+								stringTypes = stringTypes.replace('_', " ")
+
+						#create the Place object containing all required values
+						newPlace = Place(placeID, placeName, rating, openNow, photoRef, photoURL, stringTypes)
+
+						#add to array to be displayed
+						shortlistPlaces.append(newPlace)
+
 				else:
-					photoRef = 'none'
-					photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
+					stringTypes = []
+		
+					#create the Place object containing all required values
+					newPlace = Place("-", "More results found on Google Maps", "-", "-", "-", "-", stringTypes)
 
-				# get type that categorise the places
-				stringTypes = ""
-				if ("types" in items):
-					types = items["types"]
-
-					for x in types:
-						stringTypes += x + ", "
-
-					#remove last 2 characters of the string
-					stringTypes = stringTypes[:-2]
-					
-					if '_' in stringTypes:
-						stringTypes = stringTypes.replace('_', " ")
-
-				#create the Place object containing all required values
-				newPlace = Place(placeID, placeName, rating, openNow, photoRef, photoURL, stringTypes)
-
-				#add to array to be displayed
-				if (counter < 10):
-					shortlistPlaces.append(newPlace)
-					counter += 1
-				else:
-					break
+					#add to array to be displayed
+					shortlistPlaces.append(newPlace)					
+				
+				counter += 1
+				
+			else:
+				break
 				
 			if self.latitude == None or self.longitude == None:
 				data = {
@@ -198,22 +210,40 @@ class weatherPlaceRecommendations():
 			for x in range(len(shortlistPlaces)-1):
 				place = shortlistPlaces[x]
 
-				data["fulfillmentMessages"].append(
-					{
-						"card": { 
-							 "title": place.getPlaceName(),
-							 "subtitle": place.getRating() + "\n" + place.getOpenNow() + "\n" + place.getPlaceTypes(), #+ "\n" + googleLogo.show(),
-							 "imageUri": place.getPhotoURL(),
-							 "buttons": [
-							 	{
-							 		"text": "Map",
-							 		#link to open in google maps
-							 		"postback": "https://www.google.com/maps/search/?api=1&query=" + place.getPlaceName() + "&query_place_id=" + place.getPlaceID()
-							 	}
-							 ]
+				if (x != 8):
+					data["fulfillmentMessages"].append(
+						{
+							"card": { 
+								 "title": place.getPlaceName(),
+								 "subtitle": place.getRating() + "\n" + place.getOpenNow() + "\n" + place.getPlaceTypes(), #+ "\n" + googleLogo.show(),
+								 "imageUri": place.getPhotoURL(),
+								 "buttons": [
+								 	{
+								 		"text": "Map",
+								 		#link to open in google maps
+								 		"postback": "https://www.google.com/maps/search/?api=1&query=" + place.getPlaceName() + "&query_place_id=" + place.getPlaceID()
+								 	}
+								 ]
+							}
 						}
-					}
-				)
+					)
+				else:
+					data["fulfillmentMessages"].append(
+						{
+							"card": { 
+								 "title": place.getPlaceName(),
+								 "subtitle": "Powered by Google"
+								 "imageUri": "",
+								 "buttons": [
+								 	{
+								 		"text": "Map",
+								 		#link to open in google maps
+								 		"postback": "https://www.google.com/maps/search/?api=1&query=" + selectedCategory
+								 	}
+								 ]
+							}
+						}
+					)
 
 		elif (placeResult.get("status") == "ZERO_RESULTS"):
 			responseText = "No results found :("
