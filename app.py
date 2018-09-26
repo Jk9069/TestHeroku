ab13840d85cbaaa89d406bd8f9973195868b6627
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-26 22:55:39 +0800

    still finding bug source

--- app.py
+++ app.py
@@ -455,9 +455,6 @@ def getWebhookResult(postReq):
 						if '_' in stringTypes:
 							stringTypes = stringTypes.replace('_', " ")
 
-					else:
-						stringTypes = "Highlight in Penang"
-
 					#obtain photo reference to get image to display in cards
 					if ("photos" in items):
 						photoDeets = items["photos"]
@@ -480,8 +477,12 @@ def getWebhookResult(postReq):
 						photoRef = 'none'
 						photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
 
-					if photoURL == "":
-						photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
+			
+			if photoURL == "":
+				photoURL = "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png"
+
+			if stringTypes == "":
+				stringTypes = "Highlight in Penang"
 
 			showMap = "https://www.google.com/maps/search/?api=1&query=" + highlight
 
@@ -502,47 +503,47 @@ def getWebhookResult(postReq):
 				}
 			)
 
-		# this section is for LINE platform
-		lineData = {
-			"payload": {
-				"line":{
-					"type": "template",
-					"altText": "Highlights to visit in Penang.",
-					"template": {
-						"type": "carousel",
-						"columns": [
+		# # this section is for LINE platform
+		# lineData = {
+		# 	"payload": {
+		# 		"line":{
+		# 			"type": "template",
+		# 			"altText": "Highlights to visit in Penang.",
+		# 			"template": {
+		# 				"type": "carousel",
+		# 				"columns": [
 							
-						],
+		# 				],
 						
-						"imageAspectRatio": "rectangle",
-						"imageSize": "cover"
-					}
-				}
-			}				
-		}
+		# 				"imageAspectRatio": "rectangle",
+		# 				"imageSize": "cover"
+		# 			}
+		# 		}
+		# 	}				
+		# }
 
-		lineCarousel = lineData["payload"]["line"]["template"]["columns"]
+		# lineCarousel = lineData["payload"]["line"]["template"]["columns"]
 
-		for x in range(len(shortlistHighlights)):
-			highlightItem = shortlistHighlights[x]
+		# for x in range(len(shortlistHighlights)):
+		# 	highlightItem = shortlistHighlights[x]
 
-			lineCarousel.append(
-				{
-					"thumbnailImageUrl": photoURL,
-					"imageBackgroundColor": "#FFFFFF",
-					"title": highlightItem[:40],
-					"text": stringTypes[:60],
-					"actions": [
-						{
-							"type": "uri",
-							"label": "Map",
-							"uri": showMap.replace(' ', '+')
-						}
-					]
-				}
-			)				
+		# 	lineCarousel.append(
+		# 		{
+		# 			"thumbnailImageUrl": photoURL,
+		# 			"imageBackgroundColor": "#FFFFFF",
+		# 			"title": highlightItem[:40],
+		# 			"text": stringTypes[:60],
+		# 			"actions": [
+		# 				{
+		# 					"type": "uri",
+		# 					"label": "Map",
+		# 					"uri": showMap.replace(' ', '+')
+		# 				}
+		# 			]
+		# 		}
+		# 	)				
 
-		data["fulfillmentMessages"].append(lineData)
+		# data["fulfillmentMessages"].append(lineData)
 
 		data["fulfillmentMessages"].append(
 			{
