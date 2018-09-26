b378255d2157d155a6b3faece0293cd6e352fb62
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-26 23:08:54 +0800

    character limit

--- eventFinder.py
+++ eventFinder.py
@@ -115,7 +115,7 @@ class eventFinder():
 					{
 						"card": { 
 							 "title": event.getEventName(),
-							 "subtitle": event.getEventVenue() + "\n" + event.getEventDateTime() + "\n" + "Powered by Eventful",
+							 "subtitle": event.getEventVenue() + "\n" + event.getEventDateTime(),
 							 # "imageUri": event.getImgUrl(),
 							 "imageUri": "https://s3.amazonaws.com/mashape-production-logos/apis/53aa61b1e4b0a798dbd1c000_medium",
 							 "buttons": [
