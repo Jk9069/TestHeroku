5fbdef90096559e8c5d1353d62477b6ed1b14080
Jk9069 <cheok.jk9069@Gmail.com>
2018-09-26 16:31:03 +0800

    start with original template

--- eventFinder.py
+++ eventFinder.py
@@ -174,46 +174,31 @@ class eventFinder():
 							# 		"uri": "https://www.google.com"
 							# 	}
 							# ]
-
-							"type": "template",
-							"altText": "this is a carousel template",
-							"template": {
-								"type": "carousel",
-								"columns": [
+							{
+								"thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+								"imageBackgroundColor": "#FFFFFF",
+								"title": "this is menu",
+								"text": "description",
+								"actions": [
 									{
-										"thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-										"imageBackgroundColor": "#FFFFFF",
-										"title": "this is menu",
-										"text": "description",
-										"actions": [
-											{
-												"type": "postback",
-												"label": "Buy",
-												"data": "action=buy&itemid=111"
-											}
-										]
-									},
+										"type": "postback",
+										"label": "Buy",
+										"data": "action=buy&itemid=111"
+									}
+								]
+							},
+							{
+								"thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+								"imageBackgroundColor": "#000000",
+								"title": "this is menu",
+								"text": "description",
+								"actions": [
 									{
-										"thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-										"imageBackgroundColor": "#000000",
-										"title": "this is menu",
-										"text": "description",
-										"defaultAction": {
-										"type": "uri",
-										"label": "View detail",
-										"uri": "http://example.com/page/222"
-									},
-									"actions": [
-										{
-											"type": "postback",
-											"label": "Buy",
-											"data": "action=buy&itemid=222"
-										}
-									]
-								}
-							],
-							"imageAspectRatio": "rectangle",
-							"imageSize": "cover"
+										"type": "postback",
+										"label": "Buy",
+										"data": "action=buy&itemid=222"
+									}
+								]
 							}
 						}
 					)
